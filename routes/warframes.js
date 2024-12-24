b66ae355f14499c21d406b557018e06a946a3bbf
NithilanV <nithilan.varadharajan@gmail.com>
2024-12-24 12:29:46 +0000

    Added more methods

--- routes/warframes.js
+++ routes/warframes.js
@@ -5,10 +5,36 @@ const router = express.Router()
 // GET All Warframes
 router.get("/", (req, res) => {
     res.status(200).send({
-        mssg: "IT WORKED"
+        mssg: "IT GOT ALL"
     })
 })
 
+// GET one
+router.get("/:id", (req, res) => {
+    res.status(200).send({
+        mssg: "IT GOT ONE"
+    })
+})
+
+// POST one
+router.post("/", (req, res) => {
+    res.status(200).send({
+        mssg: "IT POSTED ONE"
+    })
+})
+// DELETE one
+router.delete("/:id", (req, res) => {
+    res.status(200).send({
+        mssg: "IT DELETED ONE"
+    })
+})
+
+// PATCH one
+router.patch("/:id", (req, res) => {
+    res.status(200).send({
+        mssg: "IT PATCHED ONE"
+    })
+})
 
 // Exports
 module.exports = router
